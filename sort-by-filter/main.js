4ce961eeddbf3da2cca476d937ea667531b22d25
maxime-fleury <m.fleury@codeur.online>
2021-07-02 15:44:48 +0200

    refractor

--- sort-by-filter/main.js
+++ sort-by-filter/main.js
@@ -1,15 +1,18 @@
+//definir tout les boutons
 all_btn = document.getElementById("all_btn");
 summer_btn = document.getElementById("summer_btn");
 autumn_btn = document.getElementById("autumn_btn");
 winter_btn = document.getElementById("winter_btn");
 spring_btn = document.getElementById("spring_btn");
+
+//selectionner le bouton "all"
 all_btn.style.background = "green";
 document.body.style.background = "white";
+
 all_btn.addEventListener("click", function(){
     changeActif("all_btn");
     showAllpics();
     document.body.style.background = "white";
-
 });
 summer_btn.addEventListener("click", function(){
     changeActif("summer_btn");
@@ -34,7 +37,6 @@ winter_btn.addEventListener("click", function(){
     hide("autumn");
     hide("spring");
     document.body.style.background = "#4A82C0";
-
 });
 spring_btn.addEventListener("click", function(){
     changeActif("spring_btn");
@@ -43,26 +45,23 @@ spring_btn.addEventListener("click", function(){
     hide("winter");
     hide("autumn");
     document.body.style.background = "#445415";
-
 });
+//afficher toutes les images
 function showAllpics(){
     all_imgs = document.querySelectorAll("#imgs img");
-    nb = all_imgs.length;
-    for(i=0;i<nb;i++){
+    for(i=0;i < all_imgs.length; i++)
         all_imgs[i].style.display = "block";
-    }
 }
+//cacher les images qui on une certaine class
 function hide(tclass){
     all_imgs = document.querySelectorAll("#imgs img."+tclass);
-    nb = all_imgs.length;
-    for(i=0;i<nb;i++){
+    for(i=0;i < btns.length; i++)
         all_imgs[i].style.display = "none";
-    }
 }
+//changer le boutons actif
 function changeActif(tid){
     btns = document.querySelectorAll("button");
-    nb = btns.length;
-    for(i=0;i<nb;i++){
+    for(i=0;i < btns.length; i++){
         btns[i].style.background = "red";
 	if(btns[i].id == tid)
 		 btns[i].style.background = "green";
