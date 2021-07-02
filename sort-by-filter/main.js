1bb51987a178d1e50dea2ef4eacb99a635e3753c
maxime-fleury <m.fleury@codeur.online>
2021-07-02 09:43:07 +0200

    added bonus

--- sort-by-filter/main.js
+++ sort-by-filter/main.js
@@ -4,27 +4,32 @@ autumn_btn = document.getElementById("autumn_btn");
 winter_btn = document.getElementById("winter_btn");
 spring_btn = document.getElementById("spring_btn");
 all_btn.addEventListener("click", function(){
+    changeActif("all_btn");
     showAllpics();
 });
 summer_btn.addEventListener("click", function(){
+    changeActif("summer_btn");
     showAllpics();
     hide("autumn");
     hide("winter");
     hide("spring");
 });
 autumn_btn.addEventListener("click", function(){
+    changeActif("autumn_btn");
     showAllpics();
     hide("summer");
     hide("winter");
     hide("spring");
 });
 winter_btn.addEventListener("click", function(){
+    changeActif("winter_btn");
     showAllpics();
     hide("summer");
     hide("autumn");
     hide("spring");
 });
 spring_btn.addEventListener("click", function(){
+    changeActif("spring_btn");
     showAllpics();
     hide("summer");
     hide("winter");
@@ -42,5 +47,14 @@ function hide(tclass){
     nb = all_imgs.length;
     for(i=0;i<nb;i++){
         all_imgs[i].style.display = "none";
+    }
+}
+function changeActif(tid){
+    btns = document.querySelectorAll("button");
+    nb = btns.length;
+    for(i=0;i<nb;i++){
+        btns[i].style.background = "";
+	if(btns[i].id == tid)
+		 btns[i].style.background = "red";
     }
 }
