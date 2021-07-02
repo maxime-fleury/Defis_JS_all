2903b8e596ef7c1c3ffb9ab82fe4f8b493ae1c5c
maxime-fleury <m.fleury@codeur.online>
2021-07-02 14:42:37 +0200

    style improvements

--- sort-by-filter/main.js
+++ sort-by-filter/main.js
@@ -3,9 +3,13 @@ summer_btn = document.getElementById("summer_btn");
 autumn_btn = document.getElementById("autumn_btn");
 winter_btn = document.getElementById("winter_btn");
 spring_btn = document.getElementById("spring_btn");
+all_btn.style.background = "green";
+document.body.style.background = "white";
 all_btn.addEventListener("click", function(){
     changeActif("all_btn");
     showAllpics();
+    document.body.style.background = "white";
+
 });
 summer_btn.addEventListener("click", function(){
     changeActif("summer_btn");
@@ -13,6 +17,7 @@ summer_btn.addEventListener("click", function(){
     hide("autumn");
     hide("winter");
     hide("spring");
+    document.body.style.background = "#FFA500";
 });
 autumn_btn.addEventListener("click", function(){
     changeActif("autumn_btn");
@@ -20,6 +25,7 @@ autumn_btn.addEventListener("click", function(){
     hide("summer");
     hide("winter");
     hide("spring");
+    document.body.style.background = "#A84B28";
 });
 winter_btn.addEventListener("click", function(){
     changeActif("winter_btn");
@@ -27,6 +33,8 @@ winter_btn.addEventListener("click", function(){
     hide("summer");
     hide("autumn");
     hide("spring");
+    document.body.style.background = "#4A82C0";
+
 });
 spring_btn.addEventListener("click", function(){
     changeActif("spring_btn");
@@ -34,6 +42,8 @@ spring_btn.addEventListener("click", function(){
     hide("summer");
     hide("winter");
     hide("autumn");
+    document.body.style.background = "#445415";
+
 });
 function showAllpics(){
     all_imgs = document.querySelectorAll("#imgs img");
@@ -53,8 +63,8 @@ function changeActif(tid){
     btns = document.querySelectorAll("button");
     nb = btns.length;
     for(i=0;i<nb;i++){
-        btns[i].style.background = "";
+        btns[i].style.background = "red";
 	if(btns[i].id == tid)
-		 btns[i].style.background = "red";
+		 btns[i].style.background = "green";
     }
 }
